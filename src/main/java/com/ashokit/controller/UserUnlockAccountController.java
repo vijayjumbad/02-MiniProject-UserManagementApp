b50f0228545bc847d7d5c67a62790653a1a0e950
vijayjumbad <vijayjumbad2016@gmail.com>
2020-12-26 16:58:02 +0530

    userUnlockAcoountCOntroller-unlockAccount() commit

--- src/main/java/com/ashokit/controller/UserUnlockAccountController.java
+++ src/main/java/com/ashokit/controller/UserUnlockAccountController.java
@@ -1,9 +1,13 @@
 package com.ashokit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ashokit.entity.UserMaster;
 import com.ashokit.service.UserMgmtServiceImpl;
 
 @RestController
@@ -11,6 +15,14 @@ public class UserUnlockAccountController {
 	 @Autowired
 	private UserMgmtServiceImpl service;
 	 
-	 public ResponseEntity<T>
+	 @PostMapping("unlockAcn")
+	 public ResponseEntity<UserMaster> unlockUserAccount(@RequestParam("email") String email,@RequestParam("password") String password){
+		
+		 //user service
+		 boolean unlockAccount = service.unlockAccount(email, password);
+		 
+		 return new ResponseEntity<UserMaster>(HttpStatus.CREATED);
+		 
+	 }
 
 }
